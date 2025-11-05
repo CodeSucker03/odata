43958c434291392ba6273fc2c74292b82c32ede8
duclc <primary.lechiduc@gmail.com>
2025-11-05 10:21:13 +0700

    update

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,40 +1,308 @@
+import type { FilterPayload } from "base/types/filter";
+import type { Dict } from "base/types/utils";
+import { noop } from "base/utils/shared";
+import type DynamicPage from "sap/f/DynamicPage";
+import type ComboBox from "sap/m/ComboBox";
+import type DatePicker from "sap/m/DatePicker";
+import type Input from "sap/m/Input";
+import type Label from "sap/m/Label";
+import type MultiComboBox from "sap/m/MultiComboBox";
+import type MultiInput from "sap/m/MultiInput";
+import type Select from "sap/m/Select";
+import type TextArea from "sap/m/TextArea";
+import type TimePicker from "sap/m/TimePicker";
+import Token from "sap/m/Token";
+import type FilterBar from "sap/ui/comp/filterbar/FilterBar";
+import type { FilterBar$FilterChangeEvent } from "sap/ui/comp/filterbar/FilterBar";
+import type FilterGroupItem from "sap/ui/comp/filterbar/FilterGroupItem";
+import PersonalizableInfo from "sap/ui/comp/smartvariants/PersonalizableInfo";
+import type SmartVariantManagement from "sap/ui/comp/smartvariants/SmartVariantManagement";
+import type View from "sap/ui/core/mvc/View";
 import type Router from "sap/ui/core/routing/Router";
+import type Table from "sap/ui/table/Table";
 import Base from "./Base.controller";
-import type { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 
 /**
  * @namespace base.controller
  */
 export default class Main extends Base {
+  private view: View;
   private router: Router;
+  private table: Table;
+  private layout: DynamicPage;
+
+  // Filters
+  private svm: SmartVariantManagement;
+  private expandedLabel: Label;
+  private snappedLabel: Label;
+  private filterBar: FilterBar;
+
+  // Fragments
 
   public override onInit(): void {
+    this.view = <View>this.getView();
     this.router = this.getRouter();
+    this.table = this.getControlById<Table>("table");
+    this.layout = this.getControlById<DynamicPage>("dynamicPage");
 
-    this.router.getRoute("RouteMain")?.attachMatched(this.onRouteMatched);
+    // Filters
+    this.svm = this.getControlById<SmartVariantManagement>("svm");
+    this.expandedLabel = this.getControlById<Label>("expandedLabel");
+    this.snappedLabel = this.getControlById<Label>("snappedLabel");
+    this.filterBar = this.getControlById<FilterBar>("filterBar");
+
+    // Filter initialize
+    this.filterBar.registerFetchData(this.fetchData);
+    this.filterBar.registerApplyData(this.applyData);
+    this.filterBar.registerGetFiltersWithValues(this.getFiltersWithValues);
+
+    this.svm.addPersonalizableControl(
+      new PersonalizableInfo({
+        type: "filterBar",
+        keyName: "table",
+        dataSource: "",
+        control: this.filterBar,
+      })
+    );
+    this.svm.initialise(noop, this.filterBar);
   }
 
-  private onRouteMatched = (event: Route$PatternMatchedEvent) => {
-    const args = event.getParameter("arguments");
+  // #region Filters
+  /**
+   * Get value fields to create new filter variant
+   */
+  private fetchData = () => {
+    return this.filterBar.getAllFilterItems(false).reduce<FilterPayload[]>((acc, item: FilterGroupItem) => {
+      const control = item.getControl();
+      const groupName = item.getGroupName();
+      const fieldName = item.getName();
 
-    console.log(args);
+      if (control) {
+        let fieldData: string | string[] = "";
+
+        switch (true) {
+          case this.isControl<Input>(control, "sap.m.Input"):
+          case this.isControl<TextArea>(control, "sap.m.TextArea"): {
+            fieldData = control.getValue();
+
+            break;
+          }
+          case this.isControl<MultiInput>(control, "sap.m.MultiInput"): {
+            fieldData = control.getTokens().map((token) => token.getKey());
+
+            break;
+          }
+          case this.isControl<DatePicker>(control, "sap.m.DatePicker"):
+          case this.isControl<TimePicker>(control, "sap.m.TimePicker"): {
+            fieldData = control.getValue();
+
+            break;
+          }
+          case this.isControl<Select>(control, "sap.m.Select"):
+          case this.isControl<ComboBox>(control, "sap.m.ComboBox"): {
+            fieldData = control.getSelectedKey();
+
+            break;
+          }
+          case this.isControl<MultiComboBox>(control, "sap.m.MultiComboBox"): {
+            fieldData = control.getSelectedKeys();
+
+            break;
+          }
+          default:
+            break;
+        }
+
+        acc.push({
+          groupName,
+          fieldName,
+          fieldData,
+        });
+      }
+
+      return acc;
+    }, []);
   };
 
-  public override onExit(): void | undefined {
-    this.router.getRoute("RouteMain")?.detachMatched(this.onRouteMatched);
+  /**
+   * Apply value fields from filter variant
+   */
+  private applyData = (data: unknown) => {
+    (<FilterPayload[]>data).forEach((item) => {
+      const { groupName, fieldName, fieldData } = item;
+
+      const control = this.filterBar.determineControlByName(fieldName, groupName);
+
+      switch (true) {
+        case this.isControl<Input>(control, "sap.m.Input"):
+        case this.isControl<TextArea>(control, "sap.m.TextArea"): {
+          control.setValue(<string>fieldData);
+
+          break;
+        }
+        case this.isControl<MultiInput>(control, "sap.m.MultiInput"): {
+          const tokens = (<string[]>fieldData).map((key) => new Token({ key, text: key }));
+
+          control.setTokens(tokens);
+
+          break;
+        }
+        case this.isControl<DatePicker>(control, "sap.m.DatePicker"):
+        case this.isControl<TimePicker>(control, "sap.m.TimePicker"): {
+          control.setValue(<string>fieldData);
+
+          break;
+        }
+        case this.isControl<Select>(control, "sap.m.Select"):
+        case this.isControl<ComboBox>(control, "sap.m.ComboBox"): {
+          control.setSelectedKey(<string>fieldData);
+
+          break;
+        }
+        case this.isControl<MultiComboBox>(control, "sap.m.MultiComboBox"): {
+          control.setSelectedKeys(<string[]>fieldData);
+
+          break;
+        }
+        default:
+          break;
+      }
+    });
+  };
+
+  // Get filters with values to display in labels
+  private getFiltersWithValues = () => {
+    return this.filterBar.getFilterGroupItems().reduce<FilterGroupItem[]>((acc, item) => {
+      const control = item.getControl();
+
+      if (control) {
+        switch (true) {
+          case this.isControl<Input>(control, "sap.m.Input"):
+          case this.isControl<TextArea>(control, "sap.m.TextArea"): {
+            const value = control.getValue();
+
+            if (value) {
+              acc.push(item);
+            }
+
+            break;
+          }
+          case this.isControl<MultiInput>(control, "sap.m.MultiInput"): {
+            const tokens = control.getTokens();
+
+            if (tokens.length) {
+              acc.push(item);
+            }
+
+            break;
+          }
+          case this.isControl<DatePicker>(control, "sap.m.DatePicker"):
+          case this.isControl<TimePicker>(control, "sap.m.TimePicker"): {
+            const value = control.getValue();
+
+            if (value) {
+              acc.push(item);
+            }
+
+            break;
+          }
+          case this.isControl<Select>(control, "sap.m.Select"):
+          case this.isControl<ComboBox>(control, "sap.m.ComboBox"): {
+            const value = control.getSelectedKey();
+
+            if (value) {
+              acc.push(item);
+            }
+
+            break;
+          }
+          case this.isControl<MultiComboBox>(control, "sap.m.MultiComboBox"): {
+            const keys = control.getSelectedKeys();
+
+            if (keys.length) {
+              acc.push(item);
+            }
+
+            break;
+          }
+          default:
+            break;
+        }
+      }
+
+      return acc;
+    }, []);
+  };
+
+  public onSelectionChange(event: FilterBar$FilterChangeEvent) {
+    this.svm.currentVariantSetModified(true);
+    this.filterBar.fireEvent("filterChange", event);
   }
 
-  public onNavigateToDetail() {
-    this.router.navTo("detail", {
-      productId: "HT-1000",
-    });
+  public onFilterChange() {
+    this.updateLabelsAndTable();
   }
 
-  public onDisplayTarget() {
-    this.displayTarget({
-      target: "processSuccess",
-      title: "Operation Successful",
-      description: "The operation was completed successfully.",
-    });
+  public onAfterVariantLoad() {
+    this.updateLabelsAndTable();
   }
+
+  private updateLabelsAndTable() {
+    const expandedLabel = this.filterBar.retrieveFiltersWithValuesAsTextExpanded();
+    const snappedLabel = this.filterBar.retrieveFiltersWithValuesAsText();
+
+    this.expandedLabel.setText(expandedLabel);
+    this.snappedLabel.setText(snappedLabel);
+
+    // this.table.setShowOverlay(true);
+  }
+
+  public getFilters() {
+    const filters = this.filterBar.getFilterGroupItems().reduce<Dict>((acc, item) => {
+      const control = item.getControl();
+      const name = item.getName();
+
+      switch (true) {
+        case this.isControl<Input>(control, "sap.m.Input"):
+        case this.isControl<TextArea>(control, "sap.m.TextArea"): {
+          const value = control.getValue();
+
+          if (value) {
+            acc[name] = value;
+          }
+
+          break;
+        }
+        case this.isControl<DatePicker>(control, "sap.m.DatePicker"):
+        case this.isControl<TimePicker>(control, "sap.m.TimePicker"): {
+          const value = control.getValue();
+
+          if (value) {
+            acc[name] = value;
+          }
+
+          break;
+        }
+        case this.isControl<Select>(control, "sap.m.Select"):
+        case this.isControl<ComboBox>(control, "sap.m.ComboBox"): {
+          const value = control.getSelectedKey();
+
+          if (value) {
+            acc[name] = value;
+          }
+
+          break;
+        }
+        default:
+          break;
+      }
+
+      return acc;
+    }, {});
+
+    console.log("Filters:", filters);
+
+    return filters;
+  }
+  // #endregion Filters
 }
