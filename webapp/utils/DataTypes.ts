bddf5554c55da32f710124d099c11c141a27664d
CodeSucker03 <khoinguyendao1210@gmail.com>
2025-12-08 20:54:40 +0700

    add more datatypes validation

--- webapp/utils/DataTypes.ts
+++ webapp/utils/DataTypes.ts
@@ -2,6 +2,8 @@ import { EdmType } from "sap/ui/export/library";
 import String from "sap/ui/model/type/String";
 import ValidateException from "sap/ui/model/ValidateException";
 import isEmail from "validator/es/lib/isEmail";
+import { isInt, isMobilePhone, isNumeric, isUUID } from "validator";
+import { isCurrency } from "validator";
 
 export type Column = {
   label?: string | undefined;
@@ -62,12 +64,77 @@ export class FieldEmail extends String {
   }
 }
 
-export class FieldPhone extends String {}
+export class FieldPhone extends String {
+  public constructor(...args: ConstructorParameters<typeof String>) {
+    super(...args);
+  }
 
-export class FieldCurrency extends String {}
+  public override validateValue(value: string): void | Promise<void> {
+    if (value !== "") {
+      if (!isMobilePhone(value, ["vi-VN", "en-US"], { strictMode: true })) {
+        throw new ValidateException("Invalid phone number");
+      }
+    }
+  }
+}
 
-export class FieldId extends String {}
+export class FieldCurrency extends String {
+  public constructor(...args: ConstructorParameters<typeof String>) {
+    super(...args);
+  }
 
-export class FieldPercentage extends String {}
+  public override validateValue(value: string): void | Promise<void> {
+    if (value !== "") {
+      if (
+        !isCurrency(value, {
+          // eslint-disable-next-line camelcase
+          allow_negative_sign_placeholder: true,
+        })
+      ) {
+        throw new ValidateException("Invalid Currency");
+      }
+    }
+  }
+}
 
-export class FieldQuantity extends String {}
+export class FieldId extends String {
+  public constructor(...args: ConstructorParameters<typeof String>) {
+    super(...args);
+  }
+
+  public override validateValue(value: string): void | Promise<void> {
+    if (value !== "") {
+      if (!isUUID(value)) {
+        throw new ValidateException("Invalid Id");
+      }
+    }
+  }
+}
+
+export class FieldPercentage extends String {
+  public constructor(...args: ConstructorParameters<typeof String>) {
+    super(...args);
+  }
+
+  public override validateValue(value: string): void | Promise<void> {
+    if (value !== "") {
+      if (!isNumeric(value)) {
+        throw new ValidateException("Invalid Id");
+      }
+    }
+  }
+}
+
+export class FieldQuantity extends String {
+  public constructor(...args: ConstructorParameters<typeof String>) {
+    super(...args);
+  }
+
+  public override validateValue(value: string): void | Promise<void> {
+    if (value !== "") {
+      if (!isInt(value, { min: 0 })) {
+        throw new ValidateException("Invalid Id");
+      }
+    }
+  }
+}
